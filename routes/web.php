5089e9cbbcfc6644d9c537cd5daad4a4f51f0189
imjohnpop <poprocsi.j@gmail.com>
2017-10-19 15:47:16 +0200

    added a few pages to create a basic website with using of laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/ocean/arctic', function () {
+    return view('/oceans/arctic');
+});
+
+Route::get('/ocean/atlantic', function () {
+    return view('/oceans/atlantic');
+});
+
+Route::get('/ocean/indian', function () {
+    return view('/oceans/indian');
+});
+
+Route::get('/ocean/pacific', function () {
+    return view('/oceans/pacific');
+});
+
+Route::get('/ocean/southern', function () {
+    return view('/oceans/southern');
 });
